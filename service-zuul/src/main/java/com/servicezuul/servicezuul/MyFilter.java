bb34d38078ec357d66afe4f35a42305fbae701f2
litongjing <little_jingxavi@163.com>
2018-03-30 16:25:12 +0800

    something change

---
+++ service-zuul/src/main/java/com/servicezuul/servicezuul/MyFilter.java
@@ -0,0 +1,52 @@
+package com.servicezuul.servicezuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @Author:LiTongjing
+ * @Description:
+ * @Date:Create by 下午3:42 2018/3/30
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        System.out.println(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null) {
+            System.out.println("token is error");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        System.out.println("ok");
+        return null;
+    }
+}
--- service-zuul/src/main/java/com/servicezuul/servicezuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/servicezuul/servicezuul/ServiceZuulApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
+@ComponentScan(value = {"com.servicezuul"})
 public class ServiceZuulApplication {
 
 	public static void main(String[] args) {
