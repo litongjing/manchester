e356ac1912ac7e872951df89339ec36c096f2453
litongjing <little_jingxavi@163.com>
2018-04-08 22:48:37 +0800

    something change

--- config-server/src/main/java/com/manchester/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/manchester/configserver/ConfigServerApplication.java
@@ -2,9 +2,20 @@ package com.manchester.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@EnableConfigServer
+@RestController
+@EnableEurekaClient
 public class ConfigServerApplication {
+
+	@RequestMapping("/status")
+	public String status(){
+		return "ok";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
